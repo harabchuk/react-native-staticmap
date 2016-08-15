530e1c69b574679939cde0223fdff1435168c3f8
Алексей Горобчук <aliaksei.harabchuk@gmail.com>
2016-08-15 13:19:53 +0300

    fixed issue with mapOverlay

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export default class YandexMapStatic extends Component {
     latDelta: PropTypes.any,
     lonDelta: PropTypes.any,
     mapType: PropTypes.oneOf(['map', 'sat']),
-    mapOverlay: PropTypes.oneOf(['skl', 'trf', 'skl,trf']),
+    mapOverlay: PropTypes.oneOf(['', 'skl', 'trf', 'skl,trf']),
     width: PropTypes.any,
     height: PropTypes.any,
     apiKey: PropTypes.string,
@@ -25,6 +25,7 @@ export default class YandexMapStatic extends Component {
     latDelta: 0.01,
     lonDelta: 0.01,
     mapType: 'map',
+    mapOverlay: '',
     markCenter: true,
     lang: 'en_US',
     apiKey: ''
@@ -52,7 +53,7 @@ export default class YandexMapStatic extends Component {
     if(p.markCenter){
       url+='&pt='+p.lon+','+p.lat+',pm2rdl';
     }
-    url = 'https://static-maps.yandex.ru/1.x/?ll=37.620070,55.753630&size=450,450&spn=0.01,0.01&l=sat&pt=37.620070,55.753630,pmwtm1~37.64,55.76363,pmwtm99&key=';
+
     return (
       <Image source={{uri:url}} style={{flex:1, height:p.height, resizeMode: 'cover'}} />
     )
